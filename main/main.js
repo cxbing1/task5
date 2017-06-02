2759d1b24f990199d11aa207b537e9407342966f
cxbing1 <983121062@qq.com>
2017-06-03 00:26:32 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,9 +1,9 @@
 module.exports = function main() {
-    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
+    var sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
 o) 最大值 = ${sequence.maxmum()}
-o)元素数量 = ${sequence.length()}
-o)平均值 = ${sequence.avgmum()}  
+o) 元素数量 = ${sequence.length()}
+o) 平均值 = ${sequence.avgmum()}  
 `);
 };
 
